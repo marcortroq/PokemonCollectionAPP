b6cf8a6a00b263b9c9ec9f415bb43398a4ae1fbb
marccrecam <120450628+marccrecam@users.noreply.github.com>
2024-04-02 17:38:55 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
 
 void main() {
   runApp(const MyApp());
@@ -40,8 +39,11 @@ class _MyHomePageState extends State<MyHomePage> {
 
   bool _mostrarTriangulo =
       true; // Variable para controlar la visibilidad del Triángulo
+
   void _moverFondo() {
+  if (_mostrarTriangulo) {
     print("Hola");
+    _abrirTeclado(); // Llama a la función para abrir el teclado
     setState(() {
       // Cambiar el color del contenedor
       _primerContainerColor = Colors.white;
@@ -51,191 +53,231 @@ class _MyHomePageState extends State<MyHomePage> {
       _imagenTopPosition = -70; // Cambiar esto a la posición deseada
     });
   }
+}
+
+void _abrirTeclado() {
+  // Enfoca en un campo de texto para abrir el teclado
+  FocusScope.of(context).requestFocus(FocusNode());
+}
+
+
+  void _volverFondo() {
+    print("Hola");
+    setState(() {
+      // Cambiar el color del contenedor
+      _primerContainerColor = const Color.fromRGBO(255, 22, 22, 1);
+      _segundoContainerColor =
+          Colors.black; // Cambia a cualquier color que desees
+      _mostrarTriangulo = true; // Ocultar el Triángulo
+      _imagenTopPosition = 220.0; // Cambiar esto a la posición deseada
+    });
+  }
 
   double _imagenTopPosition = 220.0; // Posición inicial de la imagen
   Color _primerContainerColor = const Color.fromRGBO(255, 22, 22, 1);
   Color _segundoContainerColor = Colors.black;
+  final FocusNode _emailFocus = FocusNode();
+  final FocusNode _passwordFocus = FocusNode();
+
+  @override
+  void dispose() {
+    _emailFocus.dispose();
+    _passwordFocus.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Container(
-            key: Key('primer_container'),
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-                colors: [_primerContainerColor, _segundoContainerColor],
+      body: GestureDetector(
+        onTap: () {
+          // Ocultar el teclado cuando se toca en cualquier lugar que no sea TextField
+          FocusScope.of(context).unfocus();
+          print("hola");
+          _volverFondo();
+        },
+        child: Stack(
+          children: [
+            Container(
+              key: Key('primer_container'),
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [_primerContainerColor, _segundoContainerColor],
+                ),
               ),
             ),
-          ),
-          if (_mostrarTriangulo)
+            if (_mostrarTriangulo)
+              Positioned(
+                left: 0, // Ajusta la posición izquierda según lo necesites
+                top: 60, // Ajusta la posición superior según lo necesites
+                child: Image.asset(
+                  'assets/Charizard.png', // Ruta de la imagen
+                  width: 420, // Ancho de la imagen
+                  height: 420, // Alto de la imagen
+                  fit: BoxFit
+                      .contain, // Ajusta el tamaño de la imagen según el contenedor
+                ),
+              ),
+            if (_mostrarTriangulo)
+              Positioned(
+                left: 70, // Ajusta la posición izquierda según lo necesites
+                top: 7, // Ajusta la posición superior según lo necesites
+                child: Transform.rotate(
+                  angle: 58 *
+                      3.14 /
+                      180, // Convierte grados a radianes
+                  child: Container(
+                    width: 22, // Ancho del rectángulo
+                    height: 900, // Alto del rectángulo
+                    color: Color.fromRGBO(
+                        29, 30, 29, 1), // Color del rectángulo
+                  ),
+                ),
+              ),
+            if (_mostrarTriangulo) // Condición para mostrar el Triángulo
+              CustomPaint(
+                size: Size.infinite,
+                painter: TrianglePainter(),
+              ),
             Positioned(
               left: 0, // Ajusta la posición izquierda según lo necesites
-              top: 60, // Ajusta la posición superior según lo necesites
+              top: _imagenTopPosition, // Ajusta la posición superior según lo necesites
               child: Image.asset(
-                'assets/Charizard.png', // Ruta de la imagen
-                width: 420, // Ancho de la imagen
-                height: 420, // Alto de la imagen
+                'assets/title.png', // Ruta de la segunda imagen
+                width: 400, // Ancho de la segunda imagen
+                height: 400, // Alto de la segunda imagen
                 fit: BoxFit
                     .contain, // Ajusta el tamaño de la imagen según el contenedor
               ),
             ),
-          if (_mostrarTriangulo)
             Positioned(
-              left: 70, // Ajusta la posición izquierda según lo necesites
-              top: 7, // Ajusta la posición superior según lo necesites
-              child: Transform.rotate(
-                angle: 58 * 3.14 / 180, // Convierte grados a radianes
-                child: Container(
-                  width: 22, // Ancho del rectángulo
-                  height: 900, // Alto del rectángulo
-                  color: Color.fromRGBO(29, 30, 29, 1), // Color del rectángulo
-                ),
-              ),
-            ),
-          if (_mostrarTriangulo) // Condición para mostrar el Triángulo
-            CustomPaint(
-              size: Size.infinite,
-              painter: TrianglePainter(),
-            ),
-          Positioned(
-            left: 0, // Ajusta la posición izquierda según lo necesites
-            top:
-                _imagenTopPosition, // Ajusta la posición superior según lo necesites
-            child: Image.asset(
-              'assets/title.png', // Ruta de la segunda imagen
-              width: 400, // Ancho de la segunda imagen
-              height: 400, // Alto de la segunda imagen
-              fit: BoxFit
-                  .contain, // Ajusta el tamaño de la imagen según el contenedor
-            ),
-          ),
-          Positioned(
-            left: 50, // Ajusta la posición izquierda según lo necesites
-            bottom: 50, // Ajusta la posición inferior según lo necesites
-            child: Column(
-              children: [
-                SizedBox(
-                  width: 305,
-                  height: 52, // Ancho del TextField
-                  child: TextField(
-                    onTap: _moverFondo,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                        borderRadius:
-                            BorderRadius.circular(13.0), // Bordes redondeados
-                      ),
-                      filled: true, // Habilitar el relleno
-                      fillColor: Colors.white, // Color de fondo
-                      labelText: 'Email Address / Username',
-                    ),
-                  ),
-                ),
-                SizedBox(height: 20), // Espacio entre los TextField
-                SizedBox(
-                  width: 305,
-                  height: 52, // Ancho del TextField
-                  child: TextField(
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                        borderRadius:
-                            BorderRadius.circular(13.0), // Bordes redondeados
-                      ),
-                      filled: true, // Habilitar el relleno
-                      fillColor: Colors.white,
-
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                            color: Colors.black,
-                            width: 2), // Borde oscuro cuando está seleccionado
-                        borderRadius:
-                            BorderRadius.circular(13.0), // Bordes redondeados
-                      ), // Color de fondo
-                      labelText: 'Password',
-                    ),
-                  ),
-                ),
-                SizedBox(height: 48), // Espacio entre los TextField
-                SizedBox(
-                  width: 305,
-                  height: 52, // Alto del botón
-                  child: ElevatedButton(
-                    onPressed: () {
-                      // Acción a realizar al presionar el botón
-                    },
-                    style: ElevatedButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                        borderRadius:
-                            BorderRadius.circular(13.0), // Bordes redondeados
-                      ),
-                      padding: EdgeInsets.zero,
-                      elevation: 0,
-                    ),
-                    child: Ink(
-                      decoration: BoxDecoration(
-                        gradient: LinearGradient(
-                          colors: [
-                            const Color.fromRGBO(66, 9, 9,
-                                1), // Color oscuro en el lado izquierdo
-                            const Color.fromRGBO(160, 0, 0,
-                                1), // Color más claro en el lado izquierdo
-                            const Color.fromRGBO(
-                                221, 19, 19, 1), // Color rojo en el centro
-                            const Color.fromRGBO(160, 0, 0,
-                                1), // Color más claro en el lado derecho
-                            const Color.fromRGBO(
-                                66, 9, 9, 1), // Color oscuro en el lado derecho
-                          ],
-                          begin: Alignment
-                              .centerLeft, // Comienza en el centro-izquierda
-                          end: Alignment
-                              .centerRight, // Termina en el centro-derecha
+              left: 50, // Ajusta la posición izquierda según lo necesites
+              bottom: 50, // Ajusta la posición inferior según lo necesites
+              child: Column(
+                children: [
+                  SizedBox(
+                    width: 305,
+                    height: 52, // Ancho del TextField
+                    child: TextField(                      
+                      onTap: () => _moverFondo(),
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius:
+                              BorderRadius.circular(13.0), // Bordes redondeados
                         ),
-                        borderRadius:
-                            BorderRadius.circular(13.0), // Bordes redondeados
+                        filled: true, // Habilitar el relleno
+                        fillColor: Colors.white, // Color de fondo
+                        labelText: 'Email Address / Username',
                       ),
-                      child: Container(
-                        alignment: Alignment.center,
-                        child: Text(
-                          'Sign In',
-                          style: TextStyle(
-                            color: Colors.white, // Color del texto del botón
-                            fontSize: 20, // Tamaño de la fuente
-                            fontFamily: 'RubikOne', // Familia de la fuente
+                    ),
+                  ),
+                  SizedBox(height: 20), // Espacio entre los TextField
+                  SizedBox(
+                    width: 305,
+                    height: 52, // Ancho del TextField
+                    child: TextField(                      
+                      onChanged: (_) => _moverFondo(),
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius:
+                              BorderRadius.circular(13.0), // Bordes redondeados
+                        ),
+                        filled: true, // Habilitar el relleno
+                        fillColor: Colors.white,
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              color: Colors.black,
+                              width: 2), // Borde oscuro cuando está seleccionado
+                          borderRadius:
+                              BorderRadius.circular(13.0), // Bordes redondeados
+                        ), // Color de fondo
+                        labelText: 'Password',
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 48), // Espacio entre los TextField
+                  SizedBox(
+                    width: 305,
+                    height: 52, // Alto del botón
+                    child: ElevatedButton(
+                      onPressed: () {
+                        // Acción a realizar al presionar el botón
+                      },
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius:
+                              BorderRadius.circular(13.0), // Bordes redondeados
+                        ),
+                        padding: EdgeInsets.zero,
+                        elevation: 0,
+                      ),
+                      child: Ink(
+                        decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                            colors: [
+                              const Color.fromRGBO(66, 9, 9,
+                                  1), // Color oscuro en el lado izquierdo
+                              const Color.fromRGBO(160, 0, 0,
+                                  1), // Color más claro en el lado izquierdo
+                              const Color.fromRGBO(
+                                  221, 19, 19, 1), // Color rojo en el centro
+                              const Color.fromRGBO(160, 0, 0,
+                                  1), // Color más claro en el lado derecho
+                              const Color.fromRGBO(
+                                  66, 9, 9, 1), // Color oscuro en el lado derecho
+                            ],
+                            begin:
+                                Alignment.centerLeft, // Comienza en el centro-izquierda
+                            end: Alignment
+                                .centerRight, // Termina en el centro-derecha
+                          ),
+                          borderRadius:
+                              BorderRadius.circular(13.0), // Bordes redondeados
+                        ),
+                        child: Container(
+                          alignment: Alignment.center,
+                          child: Text(
+                            'Sign In',
+                            style: TextStyle(
+                              color: Colors.white, // Color del texto del botón
+                              fontSize: 20, // Tamaño de la fuente
+                              fontFamily:
+                                  'RubikOne', // Familia de la fuente
+                            ),
                           ),
                         ),
                       ),
                     ),
                   ),
-                ),
-                SizedBox(height: 7), // Espacio entre el botón y el texto
-                RichText(
-                  text: TextSpan(
-                    text: "I’m a new user. ",
-                    style: TextStyle(
-                      color: Colors.black, // Color del texto principal (negro)
-                      fontSize: 18, // Tamaño de la fuente
-                      fontFamily: 'Roboto', // Familia de la fuente
-                    ),
-                    children: [
-                      TextSpan(
-                        text: "Sign Up",
-                        style: TextStyle(
-                          color: Colors.red, // Color de "Sign Up" (rojo)
-                          fontWeight: FontWeight.bold,
-                          fontFamily:
-                              'Roboto', // Opcional: negrita para enfatizar
-                        ),
+                  SizedBox(height: 7), // Espacio entre el botón y el texto
+                  RichText(
+                    text: TextSpan(
+                      text: "I’m a new user. ",
+                      style: TextStyle(
+                        color: Colors.black, // Color del texto principal (negro)
+                        fontSize: 18, // Tamaño de la fuente
+                        fontFamily: 'Roboto', // Familia de la fuente
                       ),
-                    ],
-                  ),
-                )
-              ],
+                      children: [
+                        TextSpan(
+                          text: "Sign Up",
+                          style: TextStyle(
+                            color: Colors.red, // Color de "Sign Up" (rojo)
+                            fontWeight: FontWeight.bold,
+                            fontFamily:
+                                'Roboto', // Opcional: negrita para enfatizar
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
