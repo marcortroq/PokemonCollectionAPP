087346f3df6ac1512d6b4b5e89fbd64353168cda
marcortroq <98543074+marcortroq@users.noreply.github.com>
2024-04-09 20:25:57 +0200

    Pokedex

--- lib/packs.dart
+++ lib/packs.dart
@@ -1,28 +1,101 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'dart:math' as math;
 
-class Packs extends StatelessWidget {
+class Packs extends StatefulWidget {
+  @override
+  _PacksState createState() => _PacksState();
+}
+
+class _PacksState extends State<Packs> {
+  bool _isCollectionSelected = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage(
-                'assets/fondosec.png'), // Reemplaza 'assets/background_image.jpg' con la ruta de tu imagen
-            fit: BoxFit.cover,
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(
+                  'assets/fondosec.png',
+                ),
+                fit: BoxFit.cover,
+              ),
+            ),
           ),
-        ),
+          _labels(context),
+        ],
       ),
     );
   }
 
-  Widget _buildButton(String text, String imagePath, BuildContext context) {
-    return Stack(
-        // Coloca aquí el contenido que desees sobre el fondo de pantalla
-        );
+  Widget _labels(BuildContext context) {
+    return Positioned(
+      top: 100,
+      left: 0,
+      right: 0,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          GestureDetector(
+            onTap: () {
+              setState(() {
+                _isCollectionSelected = true;
+              });
+            },
+            child: Container(
+              padding: EdgeInsets.only(
+                bottom: 2,
+              ),
+              decoration: BoxDecoration(
+                border: _isCollectionSelected
+                    ? Border(
+                        bottom: BorderSide(width: 2.0, color: Colors.white),
+                      )
+                    : null,
+              ),
+              child: Text(
+                "POKESTORE",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontFamily: 'sarpanch',
+                ),
+              ),
+            ),
+          ),
+          GestureDetector(
+            onTap: () {
+              setState(() {
+                _isCollectionSelected = false;
+              });
+            },
+            child: Container(
+              padding: EdgeInsets.only(
+                bottom: 2,
+              ),
+              decoration: BoxDecoration(
+                border: !_isCollectionSelected
+                    ? Border(
+                        bottom: BorderSide(width: 2.0, color: Colors.white),
+                      )
+                    : null,
+              ),
+              child: Text(
+                "MY PACKS",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontFamily: 'sarpanch',
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
 
--- lib/pokedex.dart
+++ lib/pokedex.dart
@@ -1,7 +1,12 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(MaterialApp(
+    home: Pokedex(),
+  ));
+}
 
 class Pokedex extends StatefulWidget {
   @override
@@ -9,38 +14,25 @@ class Pokedex extends StatefulWidget {
 }
 
 class _PokedexState extends State<Pokedex> {
-  bool _isCollectionSelected = true;
-  List<String> _pokemonCards = [];
+  List<Map<String, dynamic>>? _pokemonCards;
 
   @override
   void initState() {
     super.initState();
-    _mostrarColeccion();
+    _fetchPokemonCardsInRange();
   }
 
-  Future<void> _mostrarColeccion() async {
-    // Realizar una solicitud HTTP a la API para obtener las cartas de Pokémon
-    var url = Uri.parse('https://api.example.com/pokemon/cards');
+  Future<void> _fetchPokemonCardsInRange() async {
+    var url =
+        Uri.parse('http://51.141.92.127:5000/carta?start_id=15&end_id=16');
     var response = await http.get(url);
 
-    // Verificar si la solicitud fue exitosa
     if (response.statusCode == 200) {
-      // Decodificar los datos JSON de la respuesta
       var data = jsonDecode(response.body);
-
-      // Obtener las cartas de Pokémon de los datos decodificados
-      List<dynamic> cards = data['cards'];
-
-      // Limpiar la lista de cartas existente
       setState(() {
-        _pokemonCards.clear();
-        // Agregar el nombre de cada carta a la lista _pokemonCards
-        for (var card in cards) {
-          _pokemonCards.add(card['name']);
-        }
+        _pokemonCards = List<Map<String, dynamic>>.from(data['Cartas']);
       });
     } else {
-      // Si la solicitud no fue exitosa, mostrar un mensaje de error
       print('Error al cargar las cartas de Pokémon: ${response.statusCode}');
     }
   }
@@ -48,134 +40,28 @@ class _PokedexState extends State<Pokedex> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        fit: StackFit.expand,
-        children: [
-          Container(
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage(
-                  'assets/fondosec.png',
-                ),
-                fit: BoxFit.cover,
-              ),
-            ),
-          ),
-          _labels(context),
-          _showCollection(),
-        ],
+      appBar: AppBar(
+        title: Text('Pokedex'),
       ),
+      body: _pokemonCards != null
+          ? ListView.builder(
+              itemCount: _pokemonCards!.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(
+                      'ID Pokemon: ${_pokemonCards![index]['ID_POKEMON']}'),
+                  subtitle: _pokemonCards![index]['FOTO_CARTA'] != null
+                      ? Image.memory(
+                          // Decodificar la imagen en base64 y mostrarla
+                          base64Decode(_pokemonCards![index]['FOTO_CARTA']),
+                          width: 100,
+                          height: 100,
+                        )
+                      : Text('No hay imagen disponible'),
+                );
+              },
+            )
+          : Center(child: CircularProgressIndicator()),
     );
   }
-
-  Widget _labels(BuildContext context) {
-    return Positioned(
-      top: 100,
-      left: 0,
-      right: 0,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          GestureDetector(
-            onTap: () {
-              setState(() {
-                _isCollectionSelected = true;
-              });
-            },
-            child: Container(
-              padding: EdgeInsets.only(
-                  bottom:
-                      2), // Ajusta este valor para cambiar la distancia del subrayado al texto
-              decoration: BoxDecoration(
-                border: _isCollectionSelected
-                    ? Border(
-                        bottom: BorderSide(width: 2.0, color: Colors.white))
-                    : null,
-              ),
-              child: Text(
-                "COLLECTION",
-                style: TextStyle(
-                    color: Colors.white, fontSize: 20, fontFamily: 'sarpanch'),
-              ),
-            ),
-          ),
-          GestureDetector(
-            onTap: () {
-              setState(() {
-                _isCollectionSelected = false;
-              });
-            },
-            child: Container(
-              padding: EdgeInsets.only(
-                  bottom:
-                      2), // Ajusta este valor para cambiar la distancia del subrayado al texto
-              decoration: BoxDecoration(
-                border: !_isCollectionSelected
-                    ? Border(
-                        bottom: BorderSide(width: 2.0, color: Colors.white))
-                    : null,
-              ),
-              child: Text(
-                "DUPLICATES",
-                style: TextStyle(
-                    color: Colors.white, fontSize: 20, fontFamily: 'sarpanch'),
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget _showCollection() {
-    if (_isCollectionSelected) {
-      // Si la opción de colección está seleccionada, mostrar las cartas de Pokémon
-      return Positioned(
-        top: 150,
-        left: 0,
-        right: 0,
-        child: Column(
-          children: [
-            Text(
-              "Colección de Pokémon",
-              style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold),
-            ),
-            Expanded(
-              child: ListView.builder(
-                itemCount: _pokemonCards.length,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    title: Text(_pokemonCards[index],
-                        style: TextStyle(color: Colors.white)),
-                  );
-                },
-              ),
-            ),
-          ],
-        ),
-      );
-    } else {
-      // Si la opción de duplicados está seleccionada, mostrar un mensaje de no implementado
-      return Positioned(
-        top: 150,
-        left: 0,
-        right: 0,
-        child: Center(
-          child: Text(
-            "Duplicados",
-            style: TextStyle(color: Colors.white, fontSize: 24),
-          ),
-        ),
-      );
-    }
-  }
-}
-
-void main() {
-  runApp(MaterialApp(
-    home: Pokedex(),
-  ));
 }
