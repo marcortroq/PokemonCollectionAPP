ffb86a3434f58f5e6e85c5764aa55d88edba3bb7
Paufarruiz <119571019+Paufarruiz@users.noreply.github.com>
2024-04-12 17:57:20 +0200

    Update guardar carta

--- lib/result_screen.dart
+++ lib/result_screen.dart
@@ -1,46 +1,120 @@
+import 'dart:async';
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:provider/provider.dart';
-import 'usuario.dart'; // Importa la clase Usuario
-import 'usuario_provider.dart'; // Importa el UsuarioProvider
- // Importa el UsuarioProvider
+import 'usuario.dart';
+import 'usuario_provider.dart';
 
-class ResultScreen extends StatelessWidget {
+class ResultScreen extends StatefulWidget {
   final String text;
-  final List<String> pokemonNames;
 
-  ResultScreen({Key? key, required this.text}) :
-    pokemonNames = [
-      'Bulbasaur', 'Ivysaur', 'Venusaur', 'Charmander', 'Charmeleon', 'Charizard', 'Squirtle', 'Wartortle', 'Blastoise',
-          'Caterpie', 'Metapod', 'Butterfree', 'Weedle', 'Kakuna', 'Beedrill', 'Pidgey', 'Pidgeotto', 'Pidgeot', 'Rattata',
-          'Raticate', 'Spearow', 'Fearow', 'Ekans', 'Arbok', 'Pikachu', 'Raichu', 'Sandshrew', 'Sandslash', 'Nidoran♀',
-          'Nidorina', 'Nidoqueen', 'Nidoran♂', 'Nidorino', 'Nidoking', 'Clefairy', 'Clefable', 'Vulpix', 'Ninetales',
-          'Jigglypuff', 'Wigglytuff', 'Zubat', 'Golbat', 'Oddish', 'Gloom', 'Vileplume', 'Paras', 'Parasect', 'Venonat',
-          'Venomoth', 'Diglett', 'Dugtrio', 'Meowth', 'Persian', 'Psyduck', 'Golduck', 'Mankey', 'Primeape', 'Growlithe',
-          'Arcanine', 'Poliwag', 'Poliwhirl', 'Poliwrath', 'Abra', 'Kadabra', 'Alakazam', 'Machop', 'Machoke', 'Machamp',
-          'Bellsprout', 'Weepinbell', 'Victreebel', 'Tentacool', 'Tentacruel', 'Geodude', 'Graveler', 'Golem', 'Ponyta',
-          'Rapidash', 'Slowpoke', 'Slowbro', 'Magnemite', 'Magneton', 'Farfetch\'d', 'Doduo', 'Dodrio', 'Seel', 'Dewgong',
-          'Grimer', 'Muk', 'Shellder', 'Cloyster', 'Gastly', 'Haunter', 'Gengar', 'Onix', 'Drowzee', 'Hypno', 'Krabby',
-          'Kingler', 'Voltorb', 'Electrode', 'Exeggcute', 'Exeggutor', 'Cubone', 'Marowak', 'Hitmonlee', 'Hitmonchan',
-          'Lickitung', 'Koffing', 'Weezing', 'Rhyhorn', 'Rhydon', 'Chansey', 'Tangela', 'Kangaskhan', 'Horsea', 'Seadra',
-          'Goldeen', 'Seaking', 'Staryu', 'Starmie', 'Mr. Mime', 'Scyther', 'Jynx', 'Electabuzz', 'Magmar', 'Pinsir',
-          'Tauros', 'Magikarp', 'Gyarados', 'Lapras', 'Ditto', 'Eevee', 'Vaporeon', 'Jolteon', 'Flareon', 'Porygon',
-          'Omanyte', 'Omastar', 'Kabuto', 'Kabutops', 'Aerodactyl', 'Snorlax', 'Articuno', 'Zapdos', 'Moltres', 'Dratini',
-          'Dragonair', 'Dragonite', 'Mewtwo', 'Mew'
-    ],
-    super(key: key);
+  ResultScreen({Key? key, required this.text}) : super(key: key);
+
+  @override
+  _ResultScreenState createState() => _ResultScreenState();
+}
+
+class _ResultScreenState extends State<ResultScreen> {
+  bool _showCardImage = false;
+
+  final List<String> pokemonNames = [
+    'Bulbasaur', 'Ivysaur', 'Venusaur', 'Charmander', 'Charmeleon', 'Charizard', 'Squirtle', 'Wartortle', 'Blastoise',
+    'Caterpie', 'Metapod', 'Butterfree', 'Weedle', 'Kakuna', 'Beedrill', 'Pidgey', 'Pidgeotto', 'Pidgeot', 'Rattata',
+    'Raticate', 'Spearow', 'Fearow', 'Ekans', 'Arbok', 'Pikachu', 'Raichu', 'Sandshrew', 'Sandslash', 'Nidoran♀',
+    'Nidorina', 'Nidoqueen', 'Nidoran♂', 'Nidorino', 'Nidoking', 'Clefairy', 'Clefable', 'Vulpix', 'Ninetales',
+    'Jigglypuff', 'Wigglytuff', 'Zubat', 'Golbat', 'Oddish', 'Gloom', 'Vileplume', 'Paras', 'Parasect', 'Venonat',
+    'Venomoth', 'Diglett', 'Dugtrio', 'Meowth', 'Persian', 'Psyduck', 'Golduck', 'Mankey', 'Primeape', 'Growlithe',
+    'Arcanine', 'Poliwag', 'Poliwhirl', 'Poliwrath', 'Abra', 'Kadabra', 'Alakazam', 'Machop', 'Machoke', 'Machamp',
+    'Bellsprout', 'Weepinbell', 'Victreebel', 'Tentacool', 'Tentacruel', 'Geodude', 'Graveler', 'Golem', 'Ponyta',
+    'Rapidash', 'Slowpoke', 'Slowbro', 'Magnemite', 'Magneton', 'Farfetch\'d', 'Doduo', 'Dodrio', 'Seel', 'Dewgong',
+    'Grimer', 'Muk', 'Shellder', 'Cloyster', 'Gastly', 'Haunter', 'Gengar', 'Onix', 'Drowzee', 'Hypno', 'Krabby',
+    'Kingler', 'Voltorb', 'Electrode', 'Exeggcute', 'Exeggutor', 'Cubone', 'Marowak', 'Hitmonlee', 'Hitmonchan',
+    'Lickitung', 'Koffing', 'Weezing', 'Rhyhorn', 'Rhydon', 'Chansey', 'Tangela', 'Kangaskhan', 'Horsea', 'Seadra',
+    'Goldeen', 'Seaking', 'Staryu', 'Starmie', 'Mr. Mime', 'Scyther', 'Jynx', 'Electabuzz', 'Magmar', 'Pinsir',
+    'Tauros', 'Magikarp', 'Gyarados', 'Lapras', 'Ditto', 'Eevee', 'Vaporeon', 'Jolteon', 'Flareon', 'Porygon',
+    'Omanyte', 'Omastar', 'Kabuto', 'Kabutops', 'Aerodactyl', 'Snorlax', 'Articuno', 'Zapdos', 'Moltres', 'Dratini',
+    'Dragonair', 'Dragonite', 'Mewtwo', 'Mew'
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    int foundPokemonIndex = pokemonNames.indexWhere((pokemon) => widget.text.contains(pokemon));
+
+    return Scaffold(
+      appBar: null,
+      body: GestureDetector(
+        onTap: () {
+          _showSaveConfirmation(context, foundPokemonIndex);
+        },
+        child: Stack(
+          children: [
+            Container(
+              constraints: BoxConstraints.expand(),
+              child: Image.asset(
+                'assets/fondo_ocr.jpg',
+                fit: BoxFit.cover,
+              ),
+            ),
+            Center(
+              child: _showCardImage
+                  ? AnimatedOpacity(
+                      opacity: 1.0,
+                      duration: Duration(seconds: 3),
+                      child: Image.asset(
+                        'assets/pokeball.png',
+                        height: 200,
+                        width: 200,
+                      ),
+                    )
+                  : (foundPokemonIndex != -1
+                      ? FutureBuilder<String?>(
+                          future: fetchCardImage(foundPokemonIndex),
+                          builder: (context, snapshot) {
+                            if (snapshot.connectionState == ConnectionState.waiting) {
+                              return Image.asset(
+                                'assets/pokemon_carga1.gif',
+                                height: 200,
+                                width: 200,
+                              );
+                            } else if (snapshot.hasError) {
+                              return Text('Error: ${snapshot.error}');
+                            } else if (snapshot.hasData) {
+                              final cardImageBase64 = snapshot.data!;
+                              final cardImageBytes = base64Decode(cardImageBase64);
+                              return GestureDetector(
+                                onTap: () {
+                                  _showSaveConfirmation(context, foundPokemonIndex);
+                                },
+                                child: AnimatedOpacity(
+                                  opacity: 1.0,
+                                  duration: Duration(seconds: 3),
+                                  child: Image.memory(cardImageBytes, fit: BoxFit.cover),
+                                ),
+                              );
+                            } else {
+                              return Text('No se encontró ninguna imagen de carta para este Pokémon');
+                            }
+                          },
+                        )
+                      : Text('No se encontró ningún Pokémon')),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 
   Future<String?> fetchCardImage(int pokemonIndex) async {
-    final pokemonId = pokemonIndex + 1; // Sumar 1 a la ID recibida
-        final response = await http.get(Uri.parse('http://20.162.113.208:5000/api/cartas/$pokemonId'));
+    final pokemonId = pokemonIndex + 1;
+    final response = await http.get(Uri.parse('http://20.162.113.208:5000/api/cartas/$pokemonId'));
 
     if (response.statusCode == 200) {
       final jsonData = json.decode(response.body);
       final fotoCarta = jsonData['foto_carta'];
 
       if (fotoCarta != null) {
-        return fotoCarta; // Retorna la imagen en base64
+        return fotoCarta;
       } else {
         throw Exception('La imagen de la carta no está disponible');
       }
@@ -56,7 +130,6 @@ class ResultScreen extends StatelessWidget {
 
       final pokemonId = foundPokemonIndex + 1;
 
-      // Mostrar diálogo de confirmación para guardar el Pokémon
       showDialog(
         context: context,
         builder: (BuildContext context) {
@@ -66,7 +139,7 @@ class ResultScreen extends StatelessWidget {
             actions: [
               TextButton(
                 onPressed: () {
-                  Navigator.of(context).pop(); // Cierra el diálogo
+                  Navigator.of(context).pop();
                 },
                 child: Text('No'),
               ),
@@ -100,7 +173,12 @@ class ResultScreen extends StatelessWidget {
                     );
                   }
 
-                  Navigator.of(context).pop(); // Cierra el diálogo
+                  // Set _showCardImage to true to trigger the animation
+                  setState(() {
+                    _showCardImage = true;
+                  });
+
+                  Navigator.of(context).pop();
                 },
                 child: Text('Sí'),
               ),
@@ -116,54 +194,4 @@ class ResultScreen extends StatelessWidget {
       );
     }
   }
-
-  @override
-  Widget build(BuildContext context) {
-    final foundPokemonIndex = pokemonNames.indexWhere((pokemon) => text.contains(pokemon));
-
-    return Scaffold(
-      appBar: null,
-      body: GestureDetector(
-        onTap: () {
-          _showSaveConfirmation(context, foundPokemonIndex);
-        },
-        child: Stack(
-          children: [
-            Container(
-              constraints: BoxConstraints.expand(),
-              child: Image.asset(
-                'assets/fondo_ocr.jpg',
-                fit: BoxFit.cover,
-              ),
-            ),
-            Center(
-              child: foundPokemonIndex != -1
-                  ? FutureBuilder<String?>(
-                      future: fetchCardImage(foundPokemonIndex),
-                      builder: (context, snapshot) {
-                        if (snapshot.connectionState == ConnectionState.waiting) {
-                          return Image.asset(
-                            'assets/pokemon_carga1.gif',
-                            height: 200,
-                            width: 200,
-                          );
-                        } else if (snapshot.hasError) {
-                          return Text('Error: ${snapshot.error}');
-                        } else if (snapshot.hasData) {
-                          final cardImageBase64 = snapshot.data!;
-                          final cardImageBytes = base64Decode(cardImageBase64);
-                          return Image.memory(cardImageBytes, fit: BoxFit.cover);
-                        } else {
-                          return Text('No se encontró ninguna imagen de carta para este Pokémon');
-                        }
-                      },
-                    )
-                  : Text('No se encontró ningún Pokémon'),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
 }
-
