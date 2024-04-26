b3c65ae852dc77bdea6be8fe01997b7587a4f9b3
Paufarruiz <119571019+Paufarruiz@users.noreply.github.com>
2024-04-26 17:44:57 +0200

    unity

--- lib/UnityGamePage.dart
+++ lib/UnityGamePage.dart
@@ -1,53 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_unity_widget/flutter_unity_widget.dart';
 
-class UnityGamePage extends StatefulWidget {
-  @override
-  _UnityGamePageState createState() => _UnityGamePageState();
+class UnityGamePage extends StatelessWidget {
+  
 }
 
-class _UnityGamePageState extends State
-{
-  late UnityWidgetController _unityWidgetController;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Unity Game Page'),
-      ),
-      body: Center(
-        child: UnityWidget(
-          onUnityCreated: onUnityCreated,
-          onUnityMessage: onUnityMessage,
-          onUnitySceneLoaded: onUnitySceneLoaded,
-          fullscreen: true,
-        ),
-      ),
-    );
-  }
-
-  void onUnityCreated(controller) {
-    this._unityWidgetController = controller;
-    // Aquí puedes enviar mensajes a Unity o realizar otras acciones cuando Unity se haya creado
-  }
-
-  void onUnityMessage(message) {
-    // Manejar mensajes recibidos desde Unity
-    print('Received message from Unity: $message');
-  }
-
-  void onUnitySceneLoaded(sceneName) {
-    // Manejar eventos de carga de escena de Unity
-    print('Unity scene loaded: $sceneName');
-  }
-
-  @override
-  void dispose() {
-    // Asegúrate de liberar recursos al finalizar
-    if (_unityWidgetController != null) {
-      _unityWidgetController.dispose();
-    }
-    super.dispose();
-  }
+void main() {
+  runApp(MaterialApp(
+    home: UnityGamePage(),
+  ));
 }
