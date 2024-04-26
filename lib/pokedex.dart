d1b7d883574dfcc37d1f0d4198f861373c1e00cd
marccrecam <120450628+marccrecam@users.noreply.github.com>
2024-04-26 19:23:21 +0200

    w

--- lib/pokedex.dart
+++ lib/pokedex.dart
@@ -542,6 +542,7 @@ class _PokedexState extends State<Pokedex> {
                                 try {
                                   await updateMonedas(idUsuario, precioTotal);
                                   print('Monedas actualizadas correctamente');
+                                  deletePokedexEntry;
                                 } catch (error) {
                                   print(
                                       'Error al actualizar las monedas: $error');
@@ -629,11 +630,25 @@ class _PokedexState extends State<Pokedex> {
         'cantidad_pokecoins': 0, // No modificamos las pokecoins en este caso
       }),
     );
-
     if (response.statusCode == 200) {
       print('Monedas actualizadas correctamente');
     } else {
       throw Exception('Failed to update monedas');
     }
   }
+
+  Future<void> deletePokedexEntry(int idPokedex) async {
+  final url = Uri.parse('http://20.162.113.208:5000/api/pokedex/delete/$idPokedex');
+  final response = await http.delete(
+    url,
+    headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+  );
+  if (response.statusCode == 200) {
+    print('La entrada de la Pokedex ha sido eliminada correctamente');
+  } else {
+    throw Exception('Error al eliminar la entrada de la Pokedex');
+  }
+}
 }
