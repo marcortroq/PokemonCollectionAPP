66ea8ebc2811e58bd66af9daa7996efa49672fdf
JoelParralejo <119571009+JoelParralejo@users.noreply.github.com>
2024-04-29 15:41:58 +0200

    Update nav_bar.dart

--- lib/nav_bar.dart
+++ lib/nav_bar.dart
@@ -252,220 +252,225 @@ class _NavBarState extends State<NavBar> {
                   },
                 ),
                 ListTile(
-  title: Text(
-    'Medals',
-    style: TextStyle(
-      color: Color.fromARGB(255, 0, 0, 0),
-      fontSize: 19,
-      fontFamily: "sarpanch",
-    ),
+                    title: Text(
+                      'Medals',
+                      style: TextStyle(
+                        color: Color.fromARGB(255, 0, 0, 0),
+                        fontSize: 19,
+                        fontFamily: "sarpanch",
+                      ),
+                    ),
+                  ),
+                  // Agrega dos columnas paralelas con texto e imagen para cada medalla
+                 Container(
+  margin: EdgeInsets.symmetric(horizontal: 10), // Margen vertical para ajustar el espacio
+  decoration: BoxDecoration(
+    color: Color.fromRGBO(255, 220, 220, 0.5), // Color de fondo rojo claro
+    borderRadius: BorderRadius.circular(15), // Bordes circulares
+  ),
+  padding: EdgeInsets.all(10), // Espaciado interno para el contenido
+  child: Row(
+    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    children: [
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          // Rainbow Badge
+          Column(
+            children: [
+              Image.asset(
+                'assets/MedallaArcoiris.png', // Ruta de la imagen de la medalla
+                width: 30, // Ajusta el tamaño de la imagen según sea necesario
+                height: 30,
+              ),
+              SizedBox(height: 9), // Ajusta el espacio entre la imagen y el texto
+              Text(
+                '- Rainbow Badge',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 14,
+                  fontFamily: "sarpanch",
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 9), // Ajusta el espacio entre las medallas
+          // Cascade Badge
+          Column(
+            children: [
+              Image.asset(
+                'assets/MedallaCascada.png', // Ruta de la imagen de la medalla
+                width: 30, // Ajusta el tamaño de la imagen según sea necesario
+                height: 30,
+              ),
+              SizedBox(height: 9), // Ajusta el espacio entre la imagen y el texto
+              Text(
+                '- Cascade Badge',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 14,
+                  fontFamily: "sarpanch",
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 9), // Ajusta el espacio entre las medallas
+          // Thunder Badge
+          Column(
+            children: [
+              Image.asset(
+                'assets/MedallaTrueno.png', // Ruta de la imagen de la medalla
+                width: 30, // Ajusta el tamaño de la imagen según sea necesario
+                height: 30,
+              ),
+              SizedBox(height: 9), // Ajusta el espacio entre la imagen y el texto
+              Text(
+                '- Thunder Badge',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 14,
+                  fontFamily: "sarpanch",
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 9), // Ajusta el espacio entre las medallas
+          // Volcano Badge
+          Column(
+            children: [
+              Image.asset(
+                'assets/MedallaVolcan.png', // Ruta de la imagen de la medalla
+                width: 30, // Ajusta el tamaño de la imagen según sea necesario
+                height: 30,
+              ),
+              SizedBox(height: 9), // Ajusta el espacio entre la imagen y el texto
+              Text(
+                '- Volcano Badge',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 14,
+                  fontFamily: "sarpanch",
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          // Boulder Badge
+          Column(
+            children: [
+              Image.asset(
+                'assets/MedallaRoca.png', // Ruta de la imagen de la medalla
+                width: 30, // Ajusta el tamaño de la imagen según sea necesario
+                height: 30,
+              ),
+              SizedBox(height: 9), // Ajusta el espacio entre la imagen y el texto
+              Text(
+                '- Boulder Badge',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 14,
+                  fontFamily: "sarpanch",
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 9), // Ajusta el espacio entre las medallas
+          // Earth Badge
+          Column(
+            children: [
+              Image.asset(
+                'assets/MedallaTierra.png', // Ruta de la imagen de la medalla
+                width: 30, // Ajusta el tamaño de la imagen según sea necesario
+                height: 30,
+              ),
+              SizedBox(height: 9), // Ajusta el espacio entre la imagen y el texto
+              Text(
+                '- Earth Badge',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 14,
+                  fontFamily: "sarpanch",
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 9), // Ajusta el espacio entre las medallas
+          // Soul Badge
+          Column(
+            children: [
+              Image.asset(
+                'assets/MedallaAlma.png', // Ruta de la imagen de la medalla
+                width: 30, // Ajusta el tamaño de la imagen según sea necesario
+                height: 30,
+              ),
+              SizedBox(height: 9), // Ajusta el espacio entre la imagen y el texto
+              Text(
+                '- Soul Badge',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 14,
+                  fontFamily: "sarpanch",
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 9), // Ajusta el espacio entre las medallas
+          // Marsh Badge
+          Column(
+            children: [
+              Image.asset(
+                'assets/MedallaPantano.png', // Ruta de la imagen de la medalla
+                width: 30, // Ajusta el tamaño de la imagen según sea necesario
+                height: 30,
+              ),
+              SizedBox(height: 9), // Ajusta el espacio entre la imagen y el texto
+              Text(
+                '- Marsh Badge',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 14,
+                  fontFamily: "sarpanch",
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    ],
   ),
 ),
-// Agrega dos columnas paralelas con texto e imagen para cada medal
-Row(
-  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-  children: [
-    Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Row(
-          children: [
-            
-            Text(
-              '- Soul Badge',
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 14,
-                fontFamily: "sarpanch",
-               
-              ),
-            ),
-            Image.asset(
-              'assets/MedallaAlma.png', // Ruta de la imagen de la medalla
-              width: 24, // Ajusta el tamaño de la imagen según sea necesario
-              height: 24,
-            ),
-            SizedBox(width: 6), // Ajusta el espacio entre la imagen y el texto
-          ],
-        ),
-        Row(
-          children: [
-            
-            Text(
-              '- Rainbow Badge',
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 14,
-                fontFamily: "sarpanch",
-                
-              ),
-            ),
-            Image.asset(
-              'assets/MedallaArcoiris.png', // Ruta de la imagen de la medalla
-              width: 24, // Ajusta el tamaño de la imagen según sea necesario
-              height: 24,
-            ),
-            SizedBox(width: 6), // Ajusta el espacio entre la imagen y el texto
-          ],
-        ),
-        Row(
-          children: [
-            
-            Text(
-              '- Cascade Badge',
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 14,
-                fontFamily: "sarpanch",
-                
-              ),
-            ),
-            Image.asset(
-              'assets/MedallaCascada.png', // Ruta de la imagen de la medalla
-              width: 24, // Ajusta el tamaño de la imagen según sea necesario
-              height: 24,
-            ),
-            SizedBox(width: 6), // Ajusta el espacio entre la imagen y el texto
-          ],
-        ),
-        Row(
-          children: [
-            
-            Text(
-              '- Marsh Badge',
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 14,
-                fontFamily: "sarpanch",
-                
-              ),
-            ),
-            Image.asset(
-              'assets/MedallaPantano.png', // Ruta de la imagen de la medalla
-              width: 24, // Ajusta el tamaño de la imagen según sea necesario
-              height: 24,
-            ),
-            SizedBox(width: 6), // Ajusta el espacio entre la imagen y el texto
-          ],
-        ),
-      ],
-    ),
-    Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Row(
-          children: [
-            
-            Text(
-              '- Boulder Badge',
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 14,
-                fontFamily: "sarpanch",
-               
-              ),
-            ),
-            Image.asset(
-              'assets/MedallaRoca.png', // Ruta de la imagen de la medalla
-              width: 24, // Ajusta el tamaño de la imagen según sea necesario
-              height: 24,
-            ),
-            SizedBox(width: 6), // Ajusta el espacio entre la imagen y el texto
-          ],
-        ),
-        Row(
-          children: [
-            
-            Text(
-              '- Earth Badge',
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 14,
-                fontFamily: "sarpanch",
-               
-              ),
-            ),
-            Image.asset(
-              'assets/MedallaTierra.png', // Ruta de la imagen de la medalla
-              width: 24, // Ajusta el tamaño de la imagen según sea necesario
-              height: 24,
-            ),
-            SizedBox(width: 6), // Ajusta el espacio entre la imagen y el texto
-          ],
-        ),
-        Row(
-          children: [
-            
-            Text(
-              '- Thunder Badge',
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 14,
-                fontFamily: "sarpanch",
-                
-              ),
-            ),
-            Image.asset(
-              'assets/MedallaTrueno.png', // Ruta de la imagen de la medalla
-              width: 24, // Ajusta el tamaño de la imagen según sea necesario
-              height: 24,
-            ),
-            SizedBox(width: 6), // Ajusta el espacio entre la imagen y el texto
-          ],
-        ),
-        Row(
-          children: [
-             // Ajusta el espacio entre la imagen y el texto
-            Text(
-              '- Volcano Badge',
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 14,
-                fontFamily: "sarpanch",
-                
-              ),
-            ),
-            Image.asset(
-              'assets/MedallaVolcan.png', // Ruta de la imagen de la medalla
-              width: 24, // Ajusta el tamaño de la imagen según sea necesario
-              height: 24,
-            ),
-            SizedBox(width: 6),
-          ],
-        ),
-      ],
-    ),
-  ],
-),
-            
-          
+                   
         ],
       ),
           ),
-          ListTile(
-            title: Row(
-              children: <Widget>[
-                Text(
-                  'Log out',
-                  style: TextStyle(
-                    color: Color.fromARGB(255, 0, 0, 0),
-                    fontSize: 19,
-                    fontFamily: "sarpanch",
-                  ),
-                ),
-                 // Ajusta el espacio entre el texto y la imagen
-                Image.asset(
-                  'assets/logout_icon.png', // Ruta de la imagen de logout
-                  width: 24, // Ajusta el tamaño de la imagen según sea necesario
-                  height: 24,
-                ),
-              ],
-            ),
-            onTap: () {
-                Navigator.push(context,
-                          MaterialPageRoute(builder: (context) => MyApp()));
-            },
-          ),
+           ListTile(
+                      title: Row(
+                        children: <Widget>[
+                          Text(
+                            'Log out',
+                            style: TextStyle(
+                              color: Color.fromARGB(255, 0, 0, 0),
+                              fontSize: 19,
+                              fontFamily: "sarpanch",
+                            ),
+                          ),
+                          // Ajusta el espacio entre el texto y la imagen
+                          Image.asset(
+                            'assets/logout_icon.png', // Ruta de la imagen de logout
+                            width: 24, // Ajusta el tamaño de la imagen según sea necesario
+                            height: 24,
+                          ),
+                        ],
+                      ),
+                      onTap: () {
+                          Navigator.push(context,
+                                    MaterialPageRoute(builder: (context) => MyApp()));
+                                  },
+                              ),
         ],
       ),
     );
